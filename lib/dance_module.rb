82082d54e8311f28fd90ae547d64d2554b512cf2
Robin Kim <rkim1214@gmail.com>
2018-11-27 02:54:43 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -5,5 +5,13 @@ module Dance
   
   def jump
     "Look how high I'm jumping!"
-  end 
+  end  
+  
+  def pirouette
+    "I'm doing a pirouette"
+  end
+ 
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end
 end 
